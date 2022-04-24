02c1a778df04cf7ac0e91e87262c2c74edcda9b0
nataliecagno <102773980+nataliecagno@users.noreply.github.com>
2022-04-24 12:38:27 -0400

    Update PyPoll_Challenge.py

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -7,7 +7,7 @@ import os
 # Add a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Add a variable to save the file to a path.
-file_to_save = os.path.join("analysis", "Election_Analysis.txt")
+file_to_save = os.path.join("analysis", "Election_Results.txt")
 
 # Initialize a total vote counter.
 total_votes = 0
